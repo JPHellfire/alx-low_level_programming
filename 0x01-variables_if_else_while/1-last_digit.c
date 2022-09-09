8624ffaa895735ed209f1d3f31f283a4cab229b5
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-09 23:41:55 +0200

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,3 +8,13 @@
 *when number is below 0: then n is negative
 * Return: (0)
 */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	return (0);
+}
