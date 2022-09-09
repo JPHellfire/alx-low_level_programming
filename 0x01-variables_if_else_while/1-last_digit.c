2da2dec3e9edc8256133928fca9f3fbffbd635d1
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 00:43:24 +0200

    .

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,19 +3,32 @@
 #include <time.h>
 
 /**
-*var n store different val when executed
-*last digit of n > 5 then string "and is greater than 5"
-*last digit of n = 0 then string "and is 0"
-*last digit of n < 6 & !=0 then string "and is less than 6 and not 0"
+* main - store different val in var when executed
+* last digit of n > 5 then string "and is greater than 5"
+* last digit of n = 0 then string "and is 0"
+* last digit of n < 6 & !=0 then string "and is less than 6 and not 0"
 * Return: (0)
 */
 
 int main(void)
 {
-	int n;
+int n;
+int lastn;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+lastn = n % 10;
+if (lastn > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n, n, lastn);
+}
+else if (lastn == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastn);
+}
+else if (lastn < 6 && lastn != 0)
+{
+printf("Last digit of %d is %d is less than 6 and not 0\n", n, lastn);
+}
+return (0);
 }
