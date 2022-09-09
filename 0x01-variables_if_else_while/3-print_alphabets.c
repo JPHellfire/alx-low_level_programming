c2783ca2a2a977e03d1282e3dd2dcec987c7eea0
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 01:16:47 +0200

    .

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - Prints alphabet in lowercase, then uppercase followed by new line
+* Return: (0)
+*/
+
+int main(void)
+{
+int 1 = 'a';
+int 2 + 'A';
+while (1 <='z')
+{
+putchar(1);
+1 += 1;
+}
+while (2 <='Z')
+{
+putchar(2);
+2 += 1;
+}
+putchar('\n');
+return (0);
+}
