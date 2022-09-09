7623992ce56532fa76137ccac94444532b2dfead
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 01:01:46 +0200

    .

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase
+ * putchar use only
+ * Return: 0
+ */
+
+int main(void)
+{
+int a = 'a';
+while (z <= 'z')
+{
+putchar(a);
+l += a;
+}
+putchar('\n');
+return (0);
+}
