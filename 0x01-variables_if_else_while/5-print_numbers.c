bf5794928de93ac4875aae6a8321b615c3643f85
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 01:54:47 +0200

    .

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* Prints single digit of base 10 starting at 0 followed by new line
+* Return: (0)
+*/
+
+int main(void)
+{
+int sindig;
+while (sindig < 10)
+{
+printf("%d", singdig);
+singdig += 1;
+}
+printf("\n");
+return (0);
+}
