d6c801a3d49d2f8e137d02d03fcb25063facfb26
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-09 23:40:50 +0200

    .

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+*main - assign random number to n var
+*number greater than 0: is positive, number = 0: then = 0
+*when number is below 0: then n is negative
+* Return: (0)
+*/
