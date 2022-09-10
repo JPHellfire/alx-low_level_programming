3992430844f1c6f665006b744a265688a62d0882
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 02:54:20 +0200

    .

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Pprints all possible combinations of single-digit numbers
+*Must be seperated by , followed by spaces
+*Must be sorted by ascending order, use putchar
+* Return: (0)
+*/
+
+int main(void)
+{
+int x = 48;
+int y = 44;
+while (x <= 57)
+{
+putchar(x);
+if (x != 57)
+{
+putchar(y);
+putchar(32);
+}
+x += 1;
+}
+putchar('\n');
+return (0);
+}
