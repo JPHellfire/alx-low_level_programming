ad689f2e557d308de602c579fc6766ca0f3664d1
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 02:49:43 +0200

    .

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Prints all numbers of base 16 using putchar
+* Return: (0)
+*/
+
+int main(void)
+{
+int x = 48;
+int y = 'a';
+while (x <= 57)
+{
+putchar(n);
+x += 1;
+}
+while (y <= 'f')
+{
+putchar(y);
+y++;
+}
+putchar('\n');
+return (0);
+}
