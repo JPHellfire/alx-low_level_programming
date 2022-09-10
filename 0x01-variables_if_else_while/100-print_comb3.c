83359cd0442fb36ecbf1d4f0398012be7ae8b25f
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-10 02:59:16 +0200

    .

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+nclude <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Prints all possible different combinations of two digits.
+* Must be seperated by , followed by space
+* 01 & 10 == 0 & 1
+* Print smalles combo of 2 digits
+* Use putchar only
+* Return: (0)
+*/
+
+int main(void)
+{
+int n1 = 48;
+int n2;
+int com = 44;
+while (n1 <= 57)
+{
+n2 = n1 + 1;
+while (n2 <= 57)
+{
+putchar(n1);
+putchar(n2);
+if (n1 != 56 || n2 != 57)
+{
+putchar(com);
+putchar(32);
+}
+n2 += 1;
+}
+n1 += 1;
+}
+putchar('\n');
+return (0);
+}
