5cf02a179646c51b742ae0ca71aea48a79be7185
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-11 18:09:40 +0200

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,24 +1,21 @@
 #include <stdio.h>
-
 /**
-* main - print string in the put function
-*
-* Description: ues the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+ * main - Entry point
+ * Define data ans their size
+ * Return: alwys 0 (Success)
+ */
 int main(void)
 {
-	char c;
-	int i;
-	long li;
-	long long lli;
-	float f;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	return (0);
+    char c;
+    float fl;
+    int i;
+    long lint;
+    long long lo;
+    /*declarations of data types above*/
+    printf(“Size of a char: %ld byte(s)\n”, sizeof(c));
+    printf(“Size of a float: %ld byte(s)\n”, sizeof(fl));
+    printf(“Size of an int: %ld byte(s)\n”, sizeof(i));
+    printf(“Size of a long int: %ld byte(s)\n”, sizeof(lint));
+    printf(“Size of a long long int: %ld bytes(s)\n”, sizeof(lo));
+    return (0);
 }
