c61659a87c7be9a33b316cd815eb64a463868140
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-11 18:16:29 +0200

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  * Define data ans their size
  * Return: alwys 0 (Success)
  */
-
 int main(void)
 {
-char c;    
-float fl;
+char c; 
 int i;
 long lint;
 long long lo;
+float fl;
 
 printf(“Size of a char: %ld byte(s)\n”, sizeof(c));
 printf(“Size of a float: %ld byte(s)\n”, sizeof(fl));
