cfb7caf1f206139cbb35fb2bba46542ad7ed52e1
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-11 17:58:03 +0200

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,13 +7,13 @@
 int main(void)
 {
     char c;
-    float 55;
+    float fl;
     int i;
     long int lint;
     long long int lo;
-    /*declarations of data types above*/
+    
     printf(“Size of a char: %ld byte(s)\n”, sizeof(c));
-    printf(“Size of a float: %ld byte(s)\n”, sizeof(55));
+    printf(“Size of a float: %ld byte(s)\n”, sizeof(fl));
     printf(“Size of an int: %ld byte(s)\n”, sizeof(i));
     printf(“Size of a long int: %ld byte(s)\n”, sizeof(lint));
     printf(“Size of a long long int: %ld bytes(s)\n”, sizeof(lo));
