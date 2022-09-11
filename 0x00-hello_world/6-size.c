a2bea238a8a6371d2400f223642c14830e58f23a
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-11 18:28:13 +0200

    New push

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -16,6 +16,6 @@ printf(“Size of a char: %ld byte(s)\n”, sizeof(c));
 printf(“Size of a float: %ld byte(s)\n”, sizeof(fl));
 printf(“Size of an int: %ld byte(s)\n”, sizeof(i));
 printf(“Size of a long int: %ld byte(s)\n”, sizeof(lint));
-printf(“Size of a long long int: %ld bytes(s)\n”, sizeof(lo));
+printf(“Size of a long long int: %ld byte(s)\n”, sizeof(lo));
 return (0);
 }
