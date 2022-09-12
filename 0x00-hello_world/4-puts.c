3d50eca83c201a0fd53827e1fc83354b54613e98
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-12 21:42:31 +0200

    .

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*	main - Print a line
+*	Return: Must use puts instead of printf
+*/
+
+int main(void)
+{
+
+puts("Programming is like building a multilingual puzzle\n");
+
+return (0);
+}
