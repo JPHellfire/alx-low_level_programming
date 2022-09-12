22184ce27f1ee81da2ec95256041d10d7fa8fbf5
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-12 15:33:34 +0200

    .

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- * Define data ans their size
- * Return: Alwys 0 (Success)
- */
+*	main - Entry point
+*	Define data ans their size
+*	Return: Alwys 0 (Success)
+*/
+
 int main(void)
 {
-	char c;
-	float f;
-	int i;
-	long l;
-	long long o;
-
-	printf("Size of a char: %ld byte(s)\n", sizeof(c));
-	printf("Size of a float: %ld byte(s)\n", sizeof(f));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
-	printf("Size of a long long int: %ld bytes(s)\n", sizeof(o));
-	return (0);
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of a int: %ld byte(s)\n", sizeof(int));
+printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(long long));
+printf("Size of a float: %ld byte(s)\n", sizeof(float));
+return (0);
 }
