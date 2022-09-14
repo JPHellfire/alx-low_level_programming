a3cdcf82996c60969d67eb54708cb137ebb0e4a8
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 11:54:50 +0200

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,30 +3,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#pragma twice 
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-void
-_putchar (char c)
+
+int main(void)
+{
+for (char c = 'a' ; c <= 'z' ; c++)
 {
 write(STDOUT_FILENO, &c, 1);
 }
-void print_alphabet(void)
-{
-char alphabet;
-alphabet = 'a';
-while (alphabet <= 'z')
-{
-_putchar(alphabet);
-alphabet++;
-}
-}
-int
-main(void)
-{
-print_alphabet();
 return (0);
 }
