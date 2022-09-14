a3dbbc5bbad81442fd5569e9a6c1abf523e89799
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 16:15:35 +0200

    3-islower

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _isalpha - checks for l&u'case
+* @c: is alphabet letter
+* Return: 1 if l&ucase & 0 otherwise
+*/
+
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
