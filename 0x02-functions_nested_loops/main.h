5305658e2f63d4beaa0258ecdc1143a5977e06aa
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 10:49:56 +0200

    3. islower

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,6 +1,6 @@
 char c, *string = "_putchar\n";
-void print_alphabet(void);
-void print_alphabet_x10(void);
+vioid printlphabet(void);
+_alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
