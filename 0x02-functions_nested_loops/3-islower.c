08df211310003196d585c546d14310e87fa922d8
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 12:16:00 +0200

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,2 @@
-#include "main.h"
 
-/**
- * print_alphabet - in lower case
- * Return 0
- */
-void print_alphabet(void)
-{
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-putchar('\n');
 }
