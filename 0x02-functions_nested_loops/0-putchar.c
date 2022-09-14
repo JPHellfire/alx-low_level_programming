fc765a3ccdf391c886dc18cc26a19bd8251ff44a
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 15:33:53 +0200

    3. islower

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,12 @@
 #include <string.h>
 #include <stdio.h>
-#include "main.h"
+#include <"main.h">
 /**
 * main - program that prints _putchar
 * Return: int
 */
 int main(void)
+char c, *string = "_putchar\n";
 {
 int counter, length = strlen(string);
 for (counter = 0 ; counter < length; counter++)
