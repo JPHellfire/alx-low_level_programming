833045e24db558a4b99226cbb63ee8463e302216
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 15:54:47 +0200

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,1 +1,18 @@
+#include "main.h"
+/**
+* _islower - checks for lowercase int in
+* @c: is alphabet letter
+* Return: 1 if lowercase & 0 for uppercase
+*/
 
+int _islower(int c)
+{
+if (c <= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
