22a23e23ebe0f89fa05365afc2fa9b26cf1b825b
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-14 11:37:55 +0200

    3. islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,14 +1,15 @@
 #include <unistd.h>
 #include "main.h"
+#pragma once
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 void
-_putchar (char c)
+_putchar (char q)
 {
-write(STDOUT_FILENO, &c, 1);
+write(STDOUT_FILENO, &q, 1);
 }
 void print_alphabet(void)
 {
