ccf231e92e3adb161189cd3a1af1b91bd0f1fb8d
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-21 03:42:18 +0200

    .

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* print_rev - prints a string
+* @s: input string to print.
+*/
+void print_rev(char *s)
+{
+int i;
+while (s[i] != '\0')
+{
+i++;
+}
+i--;
+while (s[i] != '\0')
+{
+_putchar(s[i--]);
+}
+_putchar('\n');
+}
