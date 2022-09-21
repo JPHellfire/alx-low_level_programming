6b78c776fa46b6788f8e90db697f8715c4fe554e
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-21 04:39:58 +0200

    .

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -5,15 +5,14 @@
 */
 void print_rev(char *s)
 {
-int i;
-while (s[i] != '\0')
+int i = 0;
+while (*(s + i) != '\0')
+i += 1;
+i -= 1;
+while (i >= 0)
 {
-i++;
-}
+_putchar(*(s + i));
 i--;
-while (s[i] != '\0')
-{
-_putchar(s[i--]);
 }
 _putchar('\n');
 }
