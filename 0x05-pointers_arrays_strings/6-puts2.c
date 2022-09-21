a9e6f40bccc99506e942ec6f4993cd7711f1fcf7
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-21 03:45:54 +0200

    .

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* puts2 - Prints char of string.
+* @str: input string to print.
+*/
+void puts2(char *str)
+{
+int i;
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+_putchar(str[i]);
+i++;
+}
+_putchar('\n');
+}
