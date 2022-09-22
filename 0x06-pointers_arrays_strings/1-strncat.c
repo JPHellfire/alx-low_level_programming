063ed3e27a34209ac55a30a9df5444d65040f492
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-22 02:43:30 +0200

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,1 +1,13 @@
-a
+/**
+ * _strncat - concatenates two strings
+ * @dest: pointer to the destination string
+ * @src: pointer to the source string
+ * @n: number of bytes to print
+ * Return: pointer to the resulting string dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+return (dest);
+}
