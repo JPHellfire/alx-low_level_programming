b7def4d90fe96490c762db49333ebc33b959eaeb
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-22 02:30:44 +0200

    .

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* infinite_add - Adds two numbers
+* @n1: first input string
+* @n2: second input string
+* @r: pointer to stored results
+* @size_r: requested buffer size
+* Return: pointer to stored results in buffer
+*/
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+int i, j, n;
+i = j = n = 0;
+return (r);
+}
