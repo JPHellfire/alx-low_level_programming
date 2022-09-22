ccd937d59c1bf09d6879162c8ac04ac266f19f42
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-22 02:38:47 +0200

    .

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,7 +1,22 @@
-#include "main.h"
-#include <stdio.h>
-/**
-* main - check the code
-*
-* Return: Always 0.
-*/
+1 #include "main.h"
+  2 /**
+  3 * _strcat- function of two strings.
+  4 *
+  5 * @dest: pointer to destination char
+  6 * @src: pointer to source char
+  7 * Return: char
+  8 */
+  9 char *_strcat(char *dest, char *src)
+ 10 {
+ 11 unsigned int i = 0;
+ 12 unsigned int j = 0;
+ 13 while (*(dest + i) != '\0')
+ 14 i++;
+ 15 while (*(src + j) != '\0')
+ 16 {
+ 17 *(dest + i) = *(src + j);
+ 18 i++;
+ 19 j++;
+ 20 }
+ 21 return (dest);
+ 22 }
