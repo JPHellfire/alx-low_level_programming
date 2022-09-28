1dc9f84b214e45d34e9a10151dfbfb5298c964ce
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-09-28 05:27:10 +0200

    .

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strstr -  function that locates a substring.
+ *
+ * @haystack: initial string
+ * @needle: substring
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+return (NULL);
+}
