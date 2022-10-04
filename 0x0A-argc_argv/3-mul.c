5da14332be0b784d2129b6744a1188670bf938c1
JPHellfire <jaun.nieuwenhuis@hotmail.co.za>
2022-10-04 21:26:18 +0200

    fix

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+* main - Program that takes first two integer arguments and prints the product
+* @argc: Number of command line arguments
+* @argv: Array name
+* Return: 1 if not enough arguments passed in, 0 otherwise
+*/
+int main(int argc, char *argv[])
+{
+int i, j;
+if (argc == 1 || argc == 2)
+return (0);
+}
